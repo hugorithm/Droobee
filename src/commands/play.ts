6a466145b7ea9abab4dc0dd70e1f6fca4f31da64
hugorithm <hugofsilv@gmail.com>
2021-10-19 17:58:55 +0100

    js to ts fix

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -45,14 +45,36 @@ export class PlayCommand implements Command {
     }
 
     async run(parsedUserCommand: CommandContext): Promise<void> {
-        const voiceChannel = parsedUserCommand.originalMessage.member?.voice.channel;
-        // if(!voiceChannel) parsedUserCommand.originalMessage.channel.send(`You need to be in a channel to use this command ${parsedUserCommand.originalMessage.author}`)
-        // const permissions =  voiceChannel.permissionsFor(parsedUserCommand.client.user);
-        // if(!permissions.has('CONNECT')) message.channel.send(`You don't have connect permissions ${message.author}`);
-        // if(!permissions.has('SPEAK')) message.channel.send(`You don't have speak permissions ${message.author}`);
-        // if(!parsedUserCommand.args.length) message.channel.send(`Syntax: !play <link or search query> ${message.author}`);
-        // const args = await parsedUserCommand.args;
-        
+        const voiceChannel = await parsedUserCommand.originalMessage.member!.voice.channel;
+        if(!voiceChannel) parsedUserCommand.originalMessage.channel.send(`You need to be in a channel to use this command ${parsedUserCommand.originalMessage.author}`)
+
+        const member = parsedUserCommand.originalMessage.member!;
+        const permissions =  voiceChannel!.permissionsFor(member);
+
+        if(!permissions.has('CONNECT')) parsedUserCommand.originalMessage.channel.send(`You don't have connect permissions ${parsedUserCommand.originalMessage.author}`);
+        if(!permissions.has('SPEAK')) parsedUserCommand.originalMessage.channel.send(`You don't have speak permissions ${parsedUserCommand.originalMessage.author}`);
+       
+        const args = await parsedUserCommand.args;
+        if(!args.length) parsedUserCommand.originalMessage.channel.send(`Syntax: !play <link or search query> ${parsedUserCommand.originalMessage.author}`);
+
+        // const connection = await parsedUserCommand.originalMessage.member!.voice.channel;
+        const fVideo = async (query: string) => {
+            const res = await ytSearch(query);
+            return (res.videos.length >= 1) ? res.videos[0] : null;
+        }
+
+        const video = await fVideo(args.join(' '));
+        if(video){
+            const stream = ytdl(video.url, {filter: 'audioonly'});
+            // connection.play(stream, {seek: 0, volume: 1}).on('finish', () => {
+            //     voiceChannel!.leave();
+            // });
+            await parsedUserCommand.originalMessage.reply(`Now Playing ***${video.title}***`);
+
+        } else{
+            parsedUserCommand.originalMessage.channel.send('No results were found!');
+        }
+
     }
 
     hasPermissionToRun(parsedUserCommand: CommandContext): boolean {
