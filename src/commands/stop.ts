2031b0b4e6ba0eb6b2198aae5ac56cff7f3d9ee9
hugorithm <hugofsilv@gmail.com>
2021-10-19 16:20:03 +0100

    comment bad code

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,10 +1,10 @@
-module.exports = {
-    name: 'stop',
-    description: 'stops the music and leaves the channel',
-    async execute(message, args){
-        const voiceChannel = message.member.voice.channel; 
-        if(!voiceChannel) return message.channel.send(`You need to be in a channel to use this command ${message.author}`)
-        await voiceChannel.leave();
-        await message.channel.send('Stopping :call_me:');
-    }
-}
+// module.exports = {
+//     name: 'stop',
+//     description: 'stops the music and leaves the channel',
+//     async execute(message, args){
+//         const voiceChannel = message.member.voice.channel; 
+//         if(!voiceChannel) return message.channel.send(`You need to be in a channel to use this command ${message.author}`)
+//         await voiceChannel.leave();
+//         await message.channel.send('Stopping :call_me:');
+//     }
+// }
