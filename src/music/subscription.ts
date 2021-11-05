65a9428b4d3dcb0b61c0df578912d0eda545934b
hugorithm <hugofsilv@gmail.com>
2021-11-05 19:28:45 +0000

    change from 30 min to 5min

--- src/music/subscription.ts
+++ src/music/subscription.ts
@@ -100,7 +100,7 @@ export class MusicSubscription {
 					this.stop();
 					this.voiceConnection.disconnect(); 
 					subscriptions.delete(this.voiceConnection.joinConfig.guildId);
-				}, 1.8e6); 
+				}, 3e5); 
 
 			} else if (newState.status === AudioPlayerStatus.Playing) {
 				// If the Playing state has been entered, then a new track has started playback.
